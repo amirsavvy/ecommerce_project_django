b95e8ca123c186bf1115fa14916737756278d63d
Muhammad Aamir <amirsavvy>
2019-02-06 10:05:59 +0500

    Search script part 2

--- search/views.py
+++ search/views.py
@@ -7,7 +7,27 @@ from products.models import Product
 class SearchProductView(ListView):
     model = Product
     context_object_name = 'products'
-    template_name = 'products/product_list.html'
+    template_name = 'search/view.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(SearchProductView, self).get_context_data(*args, **kwargs)
+        query = self.request.GET.get('q')
+        context['query'] = query
+        # SearchQuery.objects.create(query=query)
+        return context
+
     def get_queryset(self, *args, **kwargs):
         request = self.request
-        return Product.objects.all()
+        # Python dic
+        method_dic = request.GET
+        query = method_dic.get('q', None) # method_dic['q']
+        print(query)
+
+        if query is not None:
+            return Product.objects.filter(title__icontains=query)
+        return Product.objects.featured()
+        '''
+        icontains = field is contains this
+        iexact = field is exactly this
+
+        '''
